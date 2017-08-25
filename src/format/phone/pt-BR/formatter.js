d6d42770d7f0449913e22e625f39435085dc7b8b
Wesley <wesley.ody@syonet.com>
2017-08-25 14:36:50 -0300

    format/phone: cria formato de telefone para pt-BR

---
+++ src/format/phone/pt-BR/formatter.js
@@ -0,0 +1,24 @@
+export const format = value => {
+    if ( !value ) {
+        return "";
+    }
+
+    // Limpa o valor
+    const cleanValue = value.replace( /[^0-9]+/g, "" );
+
+    if ( cleanValue.length > 4 && cleanValue.length < 9 ) {
+        return cleanValue.substring( 0, 4 ) + "-" + cleanValue.substring( 4 );
+    } else if ( cleanValue.length > 4 && cleanValue.length < 10 ) {
+        return cleanValue.substring( 0, 5 ) + "-" + cleanValue.substring( 5 );
+    } else if ( cleanValue.length === 10 ) {
+        return "(" + cleanValue.substring( 0, 2 ) + ") "
+            + cleanValue.substring( 2, 5 ) + "-" + cleanValue.substring( 5 );
+    } else if ( cleanValue.length === 10 || cleanValue.length === 11 ) {
+        return "(" + cleanValue.substring( 0, 2 ) + ") "
+            + cleanValue.substring( 2, 7 ) + "-" + cleanValue.substring( 7 );
+    } else {
+        return cleanValue;
+    }
+};
+
+export default format;
