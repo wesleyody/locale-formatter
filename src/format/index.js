e19dab7b094db7e19ee28b573016be705a5b7e17
Wesley <wesley.ody@syonet.com>
2017-08-25 14:38:28 -0300

    format: cria indice

---
+++ src/format/index.js
@@ -0,0 +1,3 @@
+export { default as phoneFormatter } from "./phone/phone-formatter";
+export { default as ssnFormatter } from "./ssn/ssn-formatter";
+export { default as einFormatter } from "./ein/ein-formatter";
