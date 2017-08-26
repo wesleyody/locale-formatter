218aec865508e8e1365df0e85b5f4005f3c4f5e5
Wesley <wesley.ody@syonet.com>
2017-08-26 10:03:57 -0300

    format: altera localização do indice

--- src/format/index.js
+++
@@ -1,3 +0,0 @@
-export { default as phoneFormatter } from "./phone/phone-formatter";
-export { default as ssnFormatter } from "./ssn/ssn-formatter";
-export { default as einFormatter } from "./ein/ein-formatter";
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as phoneFormatter } from "./format/phone/phone-formatter";
+export { default as ssnFormatter } from "./format/ssn/ssn-formatter";
+export { default as einFormatter } from "./format/ein/ein-formatter";
