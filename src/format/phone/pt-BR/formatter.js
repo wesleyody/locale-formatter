d2088ec147f97e2ff7508c8d80814c71693bf1e0
Wesley <wesley.ody@syonet.com>
2017-11-13 23:07:00 -0200

    format/phone: corrige formato de telefones com codigo do país

--- src/format/phone/pt-BR/formatter.js
+++ src/format/phone/pt-BR/formatter.js
@@ -16,6 +16,10 @@ export const format = value => {
     } else if ( cleanValue.length === 11 ) {
         return "(" + cleanValue.substring( 0, 2 ) + ") "
             + cleanValue.substring( 2, 7 ) + "-" + cleanValue.substring( 7 );
+    } else if ( cleanValue.length === 13 ) {
+        return "+" + cleanValue.substring( 0, 2 )
+            + " (" + cleanValue.substring( 2, 4 ) + ") "
+            + cleanValue.substring( 4, 9 ) + "-" + cleanValue.substring( 9 );
     } else {
         return cleanValue;
     }
